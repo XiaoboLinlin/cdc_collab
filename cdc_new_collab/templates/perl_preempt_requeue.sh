36bfc6bc42a58edbc6aff789f33b5c2ab6dcb293
XiaoboLinlin <xiaobolin11@outlook.com>
2023-03-15 20:43:20 -0700

    add functions to do quick check

--- cdc_new_collab/templates/perl_preempt_requeue.sh
+++ cdc_new_collab/templates/perl_preempt_requeue.sh
@@ -9,7 +9,7 @@
 #SBATCH --time={{ walltime|format_timedelta }}
 #SBATCH --error=./error/error_%j.err
 #SBATCH --output=./output/run_out_%j.log 
-#SBATCH --comment=144:00:00  #desired time limit
+##SBATCH --comment=144:00:00  #desired time limit
 ##SBATCH --signal=B:USR1@60  #sig_time (60 seconds) should match your checkpoint overhead time
 ##SBATCH --open-mode=append
 
@@ -18,7 +18,7 @@ ckpt_command=
 
 # user setting and executables go here
 
-trap 'scontrol requeue ${SLURM_JOB_ID}; exit 15' 15 
+# trap 'scontrol requeue ${SLURM_JOB_ID}; exit 15' 15 
 # module load python
 conda activate myenv
 
